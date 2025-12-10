b02c82955f55a6b73202d171afebaf63fd4b5f4d
guabaaa <gdandtop950923@gmail.com>
2025-12-10 23:22:37 +0900

    슈파베이스 변수 수정

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl =
-  process.env.NEXT_PUBLIC_SUPABASE_URL || "https://placeholder.supabase.co";
+  process.env.NEXT_PUBLIC_SUPABASE_URL ||
+  "https://qtcjddvjljfhukpimdrt.supabase.co";
 const supabaseKey =
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "placeholder-key";
 
